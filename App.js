768b71c010cb7dfadfcd93826eacdc5a4be1fd7c
Brandon Ngo <okibrandon@hotmail.com>
2021-02-13 21:10:42 -0500

    fixed file structure of navbar

--- App.js
+++ App.js
@@ -1,64 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import MyTabs from "./App/routes/bottomNav";
 
-// screens
-import Fridge from "./app/views/Fridge";
-import BarcodeEntry from "./app/views/BarcodeEntry";
-import ManualEntry from "./app/views/ManualEntry";
-
-const Tab = createMaterialBottomTabNavigator();
-
-function MyTabs() {
-  return (
-    <Tab.Navigator
-      initialRouteName="Fridge"
-      activeColor="#e91e63"
-      labelStyle={{ fontSize: 12 }}
-      style={{ backgroundColor: 'tomato' }}
-    >
-      <Tab.Screen
-        name="Fridge"
-        component={Fridge}
-        options={{
-          tabBarLabel: 'Fridge',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="home" color={color} size={26} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="BarcodeEntry"
-        component={BarcodeEntry}
-        options={{
-          tabBarLabel: 'BarcodeEntry',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="bell" color={color} size={26} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="ManualEntry"
-        component={ManualEntry}
-        options={{
-          tabBarLabel: 'ManualEntry',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="account" color={color} size={26} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
-}
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <MyTabs />
-    </NavigationContainer>
+    <MyTabs />
   );
 }
 
